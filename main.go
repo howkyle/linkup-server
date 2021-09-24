c5684ff405b584952d348f3de9b81f2fd44320fc
Kyle Howard <kyle.howard@smsja.net>
2021-09-23 21:03:46 -0500

    passes config to server factory

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main //linkup main package
 import "github.com/howkyle/linkup-server/server"
 
 func main() {
-	s := server.New()
+	config := server.NewConfig("./config.yml")
+	s := server.New(config)
 	s.Init()
-	// s.ConfigureServices()
-	// s.ConfigureRoutes()
 	s.Start()
 }
