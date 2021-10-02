644b66a16c51bac6ca83428e3edde1fb75f36d3a
Kyle Howard <kyle.howard@smsja.net>
2021-10-01 23:09:02 -0500

    implement uman interface for user

--- user/user.go
+++ user/user.go
@@ -8,19 +8,30 @@ import (
 
 type User struct {
 	ID        uint `gorm:"primaryKey"`
+	Username  string
+	Email     string
+	Password  string
 	CreatedAt time.Time
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt `gorm:"index"`
 }
 
-type Service interface {
-	Register(u User) (uint, error)
-	Login() error
-	Logout() error
+func (u User) GetID() interface{} {
+	return u.ID
+}
+func (u User) GetUsername() string {
+	return u.Username
+}
+func (u User) GetPassword() string {
+	return u.Password
 }
 
-type Repo interface {
-	Add(u User) (uint, error)
-	Get(id uint) (User, error)
-	Delete(id uint) error
+func (u User) GetEmail() string {
+	return u.Email
+}
+
+type Service interface {
+	Register(u User) (uint, error)
+	Login(u User, p string) error
+	Logout() error
 }
