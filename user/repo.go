13eac924b85657fc3fc580862bb51aa9ef0891c7
Kyle Howard <kyle.howard@smsja.net>
2021-10-04 22:04:27 -0500

    implements repo retrieve

--- user/repo.go
+++ user/repo.go
@@ -4,41 +4,45 @@ import (
 	"errors"
 	"log"
 
-	"github.com/howkyle/uman"
 	"gorm.io/gorm"
 )
 
 var CreationError = errors.New("unable to create new user")
 var InvalidUser = errors.New("invalid user type passed")
+var NotFound = errors.New("user not found")
 
 type repository struct {
 	DB *gorm.DB
 }
 
-func (r repository) Create(u uman.User) (interface{}, error) {
+func (r repository) Create(u User) (interface{}, error) {
 
-	user, ok := u.(User)
-	if !ok {
-		return nil, InvalidUser
-	}
-
-	res := r.DB.Create(&user)
+	res := r.DB.Create(&u)
 	if res.Error != nil {
 		log.Println(res.Error)
 		return nil, CreationError
 	}
-	return user.ID, nil
+	return u.ID, nil
 }
 
-func (r repository) Retrieve(id interface{}) (uman.User, error) {
+func (r repository) Retrieve(u User) (User, error) {
 	//can use id or struct for gorm
-	return nil, nil
+	var user User
+	res := r.DB.Where(&u).First(&user)
+	if res.Error != nil {
+		log.Println(res.Error)
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return User{}, NotFound
+		}
+		return User{}, res.Error
+	}
+	return user, nil
 }
 
 func (r repository) Delete(id interface{}) error {
 	return nil
 }
 
-func NewRepository(db *gorm.DB) uman.UserRepository {
+func NewRepository(db *gorm.DB) Repository {
 	return repository{DB: db}
 }
