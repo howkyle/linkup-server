9f0f1329299b95cef7943e98ef04e1888abca86e
Kyle Howard <kyle.howard@smsja.net>
2021-10-04 22:03:52 -0500

    implements user login

--- user/service.go
+++ user/service.go
@@ -2,10 +2,15 @@ package user
 
 import (
 	"errors"
+	"fmt"
 	"log"
 
-	"github.com/howkyle/uman"
+	"github.com/howkyle/authman"
 )
+
+var RegistrationError = errors.New("user registration failed")
+var InvalidID = errors.New("invalid user id type")
+var LoginError = errors.New("login failure")
 
 type UserSignup struct {
 	Username string
@@ -17,17 +22,29 @@ func (u UserSignup) User() User {
 	return User{Username: u.Username, Email: u.Email, Password: u.Password}
 }
 
-var RegistrationError = errors.New("user registration failed")
-var InvalidID = errors.New("invalid user id type")
+type UserLogin struct {
+	Username string
+	Password string
+}
+
+func (ul UserLogin) User() User {
+	return User{Username: ul.Username, Password: ul.Password}
+}
 
 type service struct {
-	repo        uman.UserRepository
-	userManager uman.UserManager
-	authManager uman.AuthManager
+	repo Repository
+	// userManager uman.UserManager
+	authManager authman.AuthManager
 }
 
 func (s service) Register(u User) (uint, error) {
-	id, err := s.userManager.Create(u)
+	hashedPass, err := authman.NewUserPassCredentials(u.Username, u.Password).Hash()
+	if err != nil {
+		log.Println(err)
+		return 0, RegistrationError
+	}
+	u.Password = hashedPass
+	id, err := s.repo.Create(u)
 	if err != nil {
 		log.Println(err)
 		return 0, RegistrationError
@@ -40,10 +57,18 @@ func (s service) Register(u User) (uint, error) {
 	return uid, nil
 }
 
-func (s service) Login(u User, p string) error {
-	s.userManager.Retrieve(u.Username)
+func (s service) Login(u User) (interface{}, error) {
+	user, err := s.repo.Retrieve(User{Username: u.Username})
+	if err != nil {
+		return nil, fmt.Errorf("%v: %w", LoginError, err)
+	}
+	c := authman.NewUserPassCredentials(fmt.Sprintf("%v", user.ID), user.Password)
+	auth, err := s.authManager.Authenticate(c, u.Password)
 
-	return nil
+	if err != nil {
+		return nil, fmt.Errorf("%v: %w", LoginError, err)
+	}
+	return auth.AsCookie(), nil
 }
 
 func (s service) Logout() error {
@@ -52,6 +77,6 @@ func (s service) Logout() error {
 	return nil
 }
 
-func NewService(r uman.UserRepository, a uman.AuthManager, u uman.UserManager) Service {
-	return service{repo: r, authManager: a, userManager: u}
+func NewService(r Repository, a authman.AuthManager) Service {
+	return service{repo: r, authManager: a}
 }
