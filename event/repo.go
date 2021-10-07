ebfccc02e31ad9d35b6869ab1b8c685494c95a65
Kyle Howard <kyle-howard@outlook.com>
2021-10-06 22:25:19 -0500

    implements create method for repo

--- event/repo.go
+++ event/repo.go
@@ -1,11 +1,38 @@
 package event
 
-import "gorm.io/gorm"
+import (
+	"errors"
+
+	"gorm.io/gorm"
+)
+
+var CreationError = errors.New("failed to add event to db")
 
 type repo struct {
-	db gorm.DB
+	db *gorm.DB
 }
 
-func (r repo) Create(e Event) {
+func (r repo) Create(e Event) (uint, error) {
 
+	res := r.db.Create(&e)
+	if res.Error != nil {
+		return 0, CreationError
+	}
+	return e.ID, nil
+}
+
+func (r repo) Retrieve(e interface{}) (Event, error) {
+	return Event{}, nil
+}
+
+func (r repo) Update(e Event) error {
+	return nil
+}
+
+func (r repo) Delete(e interface{}) error {
+	return nil
+}
+
+func NewRepository(db *gorm.DB) Repository {
+	return repo{db: db}
 }
