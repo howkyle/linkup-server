6ec3517bf46e40e852a2992482a8a47ece2811d5
Kyle Howard <kyle-howard@outlook.com>
2021-10-11 23:34:03 -0500

    adds db fields

--- event/event.go
+++ event/event.go
@@ -5,16 +5,16 @@ import (
 	"errors"
 	"time"
 
-	"gorm.io/gorm"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Event struct {
-	gorm.Model
-	UserID uint
-	Title  string
+	ID     primitive.ObjectID `bson:"_id, omitempty"`
+	UserID primitive.ObjectID `bson:"user_id,omitempty"`
+	Title  string             `bson:"title,omitempty"`
 	Location
-	Time        time.Time
-	Invitations map[uint]Invitation
+	Time        time.Time           `bson:"time"`
+	Invitations map[uint]Invitation `bson:"invitations, omitempty"`
 }
 
 func (e *Event) AddInvitation(i Invitation) error {
@@ -26,18 +26,18 @@ func (e *Event) AddInvitation(i Invitation) error {
 }
 
 type Location struct {
-	Latitude  int
-	Longitude int
-	Name      string
+	Latitude  int    `bson:"latitute,omitempty"`
+	Longitude int    `bson:"longitude,omitempty"`
+	Name      string `bson:"name,omitempty"`
 }
 
 type Service interface {
-	CreateEvent(e CreateEvent) (uint, error)
+	CreateEvent(e CreateEvent) (interface{}, error)
 }
 
 type Repository interface {
 	//adds a new event record to the database
-	Create(e Event) (uint, error)
+	Create(e Event) (interface{}, error)
 	//retrieves an event record from the database
 	Retrieve(e interface{}) (Event, error)
 	//updates an existing event record
