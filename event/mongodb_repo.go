5b11456c8f4b932a1f1a6fdb71c90052d888292d
Kyle Howard <kyle-howard@outlook.com>
2021-10-13 23:53:45 -0500

    implements retrieve and update methods

--- event/mongodb_repo.go
+++ event/mongodb_repo.go
@@ -4,8 +4,10 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"log"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -27,15 +29,35 @@ func (r mongorepo) Create(e Event) (interface{}, error) {
 	return res.InsertedID, nil
 }
 
-func (r mongorepo) Retrieve(e interface{}) (Event, error) {
-	return Event{}, nil
+func (r mongorepo) Retrieve(id interface{}) (Event, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	var event Event
+	log.Printf("%v", id)
+	filter := bson.D{{Key: "_id", Value: id}}
+	err := r.db.Collection(collection).FindOne(ctx, filter).Decode(&event)
+	if err != nil {
+		return Event{}, fmt.Errorf("failed to retrieve event: %w", err)
+	}
+	return event, nil
 }
 
 func (r mongorepo) Update(e Event) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*2)
+	defer cancel()
+	filter := bson.D{{Key: "_id", Value: e.ID}}
+	update := bson.D{{Key: "$set", Value: e}}
+	res, err := r.db.Collection(collection).UpdateOne(ctx, filter, update)
+	if err != nil {
+		return fmt.Errorf("failed to update event: %w", err)
+	}
+	if res.ModifiedCount != 1 {
+		return fmt.Errorf("event document not modified")
+	}
 	return nil
 }
 
-func (r mongorepo) Delete(e interface{}) error {
+func (r mongorepo) Delete(e Event) error {
 	return nil
 }
 
