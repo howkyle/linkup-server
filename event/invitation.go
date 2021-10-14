be50102dff8d2268b68803fbbdfc2b4b7edf5629
Kyle Howard <kyle-howard@outlook.com>
2021-10-13 23:53:13 -0500

    adds bson tags

--- event/invitation.go
+++ event/invitation.go
@@ -1,7 +1,10 @@
 package event
 
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
 type Invitation struct {
-	EventID  uint
-	UserID   uint
-	Accepted bool
+	ID       primitive.ObjectID `bson:"_id,omitempty"`
+	UserID   primitive.ObjectID `bson:"user_id,omitempty"`
+	EventID  primitive.ObjectID `bson:"event_id,omitempty"`
+	Accepted bool               `bson:"accepted,omitempty"`
 }
