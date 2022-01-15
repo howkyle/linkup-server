30119974fff4da953cda4bf6440f6a69d3a9d8c4
Kyle Howard <howardka@hotmail.com>
2022-01-14 23:14:55 -0500

    removes data struct

--- event/service.go
+++ event/service.go
@@ -2,9 +2,6 @@ package event
 
 import (
 	"fmt"
-	"time"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type service struct {
@@ -12,10 +9,10 @@ type service struct {
 }
 
 //create a new event
-func (s service) CreateEvent(c CreateEvent) (interface{}, error) {
-	//validate struct then create
+func (s service) CreateEvent(c Event) (interface{}, error) {
 
-	id, err := s.repo.Create(c.Event())
+	// check if dupilcate exists
+	id, err := s.repo.Create(c)
 	if err != nil {
 		return 0, fmt.Errorf("failed to create event: %w", err)
 	}
@@ -34,21 +31,4 @@ func (s service) Event(id interface{}) (Event, error) {
 
 func NewService(r Repository) Service {
 	return service{repo: r}
-}
-
-//useCases
-
-//used when creating a new event
-type CreateEvent struct {
-	UserID       primitive.ObjectID
-	Title        string
-	Latitude     int
-	Longitude    int
-	LocationName string
-	Time         time.Time
-}
-
-//maps usecase to an event
-func (c CreateEvent) Event() Event {
-	return Event{UserID: c.UserID, Title: c.Title, Time: c.Time, Location: Location{Latitude: c.Latitude, Longitude: c.Longitude, LocationName: c.LocationName}}
 }
