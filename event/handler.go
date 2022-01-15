ae7ff8a9e2889d5a8bb2562bc4b7d89a4aa8b0cf
Kyle Howard <howardka@hotmail.com>
2022-01-14 23:13:59 -0500

    adds decode and validate and create event

--- event/handler.go
+++ event/handler.go
@@ -3,37 +3,57 @@ package event
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/howkyle/linkup-server/validation"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-//handles the creation of a new event
-func NewEventHandler(s Service) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		userid := r.Context().Value("sub")
-		var c CreateEvent
-		err := json.NewDecoder(r.Body).Decode(&c)
-		if err != nil {
-			log.Println(err)
-			http.Error(w, "failed to read body", http.StatusBadRequest)
-			return
-		}
+//used when creating a new event
+type CreateEvent struct {
+	Title        string    `json:"title" validate:"required"`
+	Latitude     int       `json:"latitude" validate:"required"`
+	Longitude    int       `json:"longitude" validate:"required"`
+	LocationName string    `json:"location_name" validate:"required"`
+	Time         time.Time `json:"time" validate:"required"`
+}
 
-		c.UserID, err = primitive.ObjectIDFromHex(fmt.Sprint(userid))
+//maps usecase to an event
+func (c CreateEvent) Event(userID primitive.ObjectID) Event {
+	return Event{UserID: userID, Title: c.Title, Time: c.Time, Location: Location{Latitude: c.Latitude, Longitude: c.Longitude, LocationName: c.LocationName}}
+}
+
+//handles the creation of a new event
+func NewEventHandler(s Service, v validation.Validator) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+
+		sub := r.Context().Value("sub")
+		userid, err := primitive.ObjectIDFromHex(fmt.Sprint(sub))
 		if err != nil {
 			log.Println(err)
 			http.Error(w, "id conversion failed", http.StatusInternalServerError)
 			return
 		}
 
-		eid, err := s.CreateEvent(c)
+		var c CreateEvent
+
+		err = decodeAndValidate(r.Body, &c, v)
 		if err != nil {
 			log.Println(err)
-			http.Error(w, "failed to create event", http.StatusInternalServerError)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
+
+		eid, err := s.CreateEvent(c.Event(userid))
+		if err != nil {
+			log.Println(err)
+			http.Error(w, fmt.Sprintf("failed to create event: %v", err), http.StatusInternalServerError)
+			return
+		}
+
 		err = json.NewEncoder(w).Encode(eid)
 		if err != nil {
 			log.Println(err)
@@ -41,4 +61,19 @@ func NewEventHandler(s Service) http.HandlerFunc {
 		}
 
 	}
+}
+
+func decodeAndValidate(body io.ReadCloser, target interface{}, v validation.Validator) error {
+	err := json.NewDecoder(body).Decode(target)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	err = v.ValidateStruct(target)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil
 }
